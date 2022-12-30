34bf6d917c74256da9add68febc4fc529360d062
Rafael Couto <rafaelscouto@gmail.com>
2022-12-30 10:27:49 -0300

    Implementation of service layer and component registration

--- src/main/java/application/resources/UserResource.java
+++ src/main/java/application/resources/UserResource.java
@@ -1,19 +1,33 @@
 package application.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import application.entities.User;
+import application.services.UserService;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value = "/users") // cria a rota
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Rafael", "rafaelscouto@gmail.com", "74988199939", "102030");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}") // add o id a rota princial
+	public ResponseEntity<User> findById(@PathVariable Long id) { // transforma o id em string
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
