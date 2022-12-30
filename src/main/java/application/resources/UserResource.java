3f44660d1f24ef8b9ce93ed8c78841eb38612c96
Rafael Couto <rafaelscouto@gmail.com>
2022-12-29 23:49:20 -0300

    init project; Implementation User entity and resource

---
+++ src/main/java/application/resources/UserResource.java
@@ -0,0 +1,19 @@
+package application.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import application.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Rafael", "rafaelscouto@gmail.com", "74988199939", "102030");
+		return ResponseEntity.ok().body(u);
+	}
+}
