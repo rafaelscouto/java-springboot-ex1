ab8c39c9b1f0811d6cfcb7c1af297889ebec215e
Rafael Couto <rafaelscouto@gmail.com>
2023-01-02 11:06:39 -0300

    Payment and Order one-to-one association

--- src/main/java/application/config/TestConfig.java
+++ src/main/java/application/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import application.entities.Category;
 import application.entities.Order;
 import application.entities.OrderItem;
+import application.entities.Payment;
 import application.entities.Product;
 import application.entities.User;
 import application.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 }
