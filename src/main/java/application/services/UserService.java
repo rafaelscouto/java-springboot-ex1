ec2ac4bf0a65d23d98a12a14d76bc5cd7f12c3ed
Rafael Couto <rafaelscouto@gmail.com>
2023-01-02 12:37:48 -0300

    Exception handling - delete

--- src/main/java/application/services/UserService.java
+++ src/main/java/application/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import application.entities.User;
 import application.repositories.UserRepository;
+import application.services.exceptions.DatabaseException;
 import application.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -42,6 +45,12 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);			
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 }
